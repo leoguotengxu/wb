5acc42e92ad2df8077196e3ef59adea9e29603bf
vagrant <vagrant@homestead>
2021-03-17 22:16:26 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -72,21 +72,7 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
     }
-    public function follow($user_ids)
-    {
-        if ( ! is_array($user_ids)) {
-            $user_ids = compact('user_ids');
-        }
-        $this->followings()->sync($user_ids, false);
-    }
 
-    public function unfollow($user_ids)
-    {
-        if ( ! is_array($user_ids)) {
-            $user_ids = compact('user_ids');
-        }
-        $this->followings()->detach($user_ids);
-    }
     public function follow($user_ids)
     {
         if ( ! is_array($user_ids)) {
